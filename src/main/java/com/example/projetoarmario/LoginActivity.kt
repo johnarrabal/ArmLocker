ef479817da953d92e5512a0f222e5e66591b27af
Joao Gabriel <arrabaljoaogabriel@gmail.com>
2024-12-10 08:43:40 -0300

    first commit

---
+++ src/main/java/com/example/projetoarmario/LoginActivity.kt
@@ -0,0 +1,33 @@
+package com.example.projetoarmario
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projetoarmario.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var databaseHelper: DatabaseHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        databaseHelper = DatabaseHelper(this)
+
+        binding.buttonLogin.setOnClickListener {
+            val email = binding.editTextEmail.text.toString()
+            val password = binding.editTextPassword.text.toString()
+
+            if (databaseHelper.checkUser(email, password)) {
+                val intent = Intent(this, UserLogActivity::class.java)
+                intent.putExtra("USER_NAME", email)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Email ou senha incorretos", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
